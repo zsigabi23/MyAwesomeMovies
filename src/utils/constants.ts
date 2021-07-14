7c875bd8eb39c1756a02f5d865b47f47f3785e38
Zolile Sigabi <39824495+zsigabi23@users.noreply.github.com>
2021-07-14 18:38:44 +0200

    updates

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,20 +6,20 @@ export const movieDetailsURL = (movieId: string) => `https://api.themoviedb.org/
 
 export const imageUrl = (path: string) => `https://www.themoviedb.org/t/p/w220_and_h330_face/${path}`;
 
-export const trelloKey = "5cd762ee55a5aa0229fbeccb276e7250" // "6eb508bda626ff893db446eff50d0066";
+export const trelloKey = "6eb508bda626ff893db446eff50d0066";
 
-export const trelloToken = "518bd6f16885085c0afe3212477e2a9c609e7894ef4e46782a80ad59576da712" // "ae4a73cb0e40c46f6e642f5f7429394534b35e3b5a4c7c21438e5389eec20497";
+export const trelloToken = "ae4a73cb0e40c46f6e642f5f7429394534b35e3b5a4c7c21438e5389eec20497";
 
-export const trelloBoardId = "5b2747fca606f9794c9a5e14" // "5a4b3c4cbe0188ca9c0b2058";
+export const trelloBoardId = "5a4b3c4cbe0188ca9c0b2058";
 
-export const trelloListId = "60ec9debb89a585f2238127c" // "5a4b3c4cbe0188ca9c0b2059";
+export const trelloListId = "5a4b3c4cbe0188ca9c0b2059";
 
-export const customFieldName = "60ec9eb37a7169426f4ad50b" // "5ffc223c1b802319cb6192fb";
+export const customFieldName = "5ffc223c1b802319cb6192fb";
 
-export const customFieldSurname = "60ec9ebe372b9c73f2015430" // "5ffc224be427094809dd7b7c";
+export const customFieldSurname = "5ffc224be427094809dd7b7c";
 
-export const customFieldEmail = "60ec9ec858a00c10f13c6e3d" // "5ffc22574a33172aa21ccda1";
+export const customFieldEmail = "5ffc22574a33172aa21ccda1";
 
-export const customFieldPhone = "60ec9ed3293ba06a2c2ad994" // "5ffc22612d199f8b0e325ef4";
+export const customFieldPhone = "5ffc22612d199f8b0e325ef4";
 
-export const customFieldMovie = "60ec9ee26c45db38e9f737bc" // "5ffc672ef7f3ca718a1a9b93";
+export const customFieldMovie = "5ffc672ef7f3ca718a1a9b93";
